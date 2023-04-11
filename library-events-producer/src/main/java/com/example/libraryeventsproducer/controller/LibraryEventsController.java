db82edda582e7620c72252d7ea152be8eb69c2b7
Emerson da ESL. Silva Lopes <emerson.lopes@ciccr-ce.interno>
2023-04-11 08:48:28 -0300

    Adicionado logs para entender o funcionamento por trás das cenas

--- library-events-producer/src/main/java/com/example/libraryeventsproducer/controller/LibraryEventsController.java
+++ library-events-producer/src/main/java/com/example/libraryeventsproducer/controller/LibraryEventsController.java
@@ -3,6 +3,7 @@ package com.example.libraryeventsproducer.controller;
 import com.example.libraryeventsproducer.domain.LibraryEvent;
 import com.example.libraryeventsproducer.producer.LibraryEventProducer;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Log4j2
 @RestController
 public class LibraryEventsController {
 
@@ -19,8 +21,10 @@ public class LibraryEventsController {
     @PostMapping("/v1/libraryevent")
     public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException {
 
+        log.info("before sendLibraryEvent");
         // invoke kafka producer
         libraryEventProducer.sendLibraryEvent(libraryEvent);
+        log.info("after sendLibraryEvent");
 
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
 
