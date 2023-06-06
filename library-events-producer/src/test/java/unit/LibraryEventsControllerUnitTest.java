48b158e6f8f4e1c70e403386bf064e55fd4b14f9
Emerson da ESL. Silva Lopes <emerson.lopes@ciccr-ce.interno>
2023-06-06 14:29:15 -0300

    Iniciado teste unitário

---
+++ library-events-producer/src/test/java/unit/LibraryEventsControllerUnitTest.java
@@ -0,0 +1,66 @@
+package unit;
+
+import com.example.libraryeventsproducer.controller.LibraryEventsController;
+import com.example.libraryeventsproducer.domain.Book;
+import com.example.libraryeventsproducer.domain.LibraryEvent;
+import com.example.libraryeventsproducer.producer.LibraryEventProducer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.doNothing;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+//@WebMvcTest(LibraryEventsController.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        classes = {com.example.libraryeventsproducer.LibraryEventsProducerApplication.class})
+@AutoConfigureMockMvc
+public class LibraryEventsControllerUnitTest {
+
+    @Autowired
+    MockMvc mockMvc;
+    @MockBean
+    LibraryEventProducer libraryEventProducer;
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    void postLibraryEvent() throws Exception {
+        //given
+        Book book = Book.builder()
+                .bookId(123)
+                .bookAuthor("Dilip")
+                .bookName("Kafka using Spring Boot")
+                .build();
+
+        LibraryEvent libraryEvent = LibraryEvent.builder()
+                .libraryEventId(null)
+                .book(book)
+                .build();
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set("content-type", MediaType.APPLICATION_JSON.toString());
+        HttpEntity<LibraryEvent> request = new HttpEntity<>(libraryEvent, httpHeaders);
+
+        doNothing().when(libraryEventProducer).sendLibraryEvent_Approach2(libraryEvent);
+
+        String json = objectMapper.writeValueAsString(libraryEvent);
+        //when
+        mockMvc.perform(post("/v1/libraryevent")
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+
+        //then
+
+    }
+}
