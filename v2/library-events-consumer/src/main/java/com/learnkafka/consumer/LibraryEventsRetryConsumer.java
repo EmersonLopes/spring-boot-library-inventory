c1e9fefc6a5fe94905cbb6b472455c5e022bcaa2
Emerson da ESL. Silva Lopes <emerson.lopes@ciccr-ce.interno>
2023-11-22 10:42:00 -0300

    Build the retryTopic listener to reprocess the message

---
+++ v2/library-events-consumer/src/main/java/com/learnkafka/consumer/LibraryEventsRetryConsumer.java
@@ -0,0 +1,26 @@
+package com.learnkafka.consumer;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.learnkafka.service.LibraryEventService;
+import lombok.extern.log4j.Log4j2;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Log4j2
+public class LibraryEventsRetryConsumer {
+
+    @Autowired
+    private LibraryEventService libraryEventService;
+
+    @KafkaListener(topics = {"${topics.retry}"}, groupId = "retry-listener-group")
+    public void onMessage(ConsumerRecord<Integer, String> consumerRecord) throws JsonProcessingException {
+        log.info("ConsumerRecord in Retry Consumer: {}", consumerRecord);
+
+        consumerRecord.headers().forEach(header -> log.info("Key: {}, value; {}", header.key(), new String(header.value())));
+
+        libraryEventService.processLibraryEvent(consumerRecord);
+    }
+}
